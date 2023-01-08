8aa8132fc7d1e460ff957bf090a4dbfb7fcd4253
Dasewasia10 <daniel.stonford10@gmail.com>
2023-01-08 20:53:39 +0800

    add image as base generator

--- src/pages/GeneratorRelationship.jsx
+++ src/pages/GeneratorRelationship.jsx
@@ -1,8 +1,8 @@
 import { Link } from "react-router-dom";
 import Slider from "../components/slider";
-import AddPP from "../components/addPP";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import bgImage from "../assets/bg-image.jpg";
 
 const GeneratorRelationship = () => {
 	const [nameInput, setNameInput] = useState("No Name");
@@ -15,15 +15,15 @@ const GeneratorRelationship = () => {
 	const url = "../config/relationshipSliderValue.json";
 
 	const changeFungsiValue = (id, value) => {
-        setSlideValue(
-            slideValue.map(item => {
-                if (item.sliderName == id) {
-                    return { ...item, "fungsiValue": value };
-                }
-                return item;
-            })
-        );
-    };
+		setSlideValue(
+			slideValue.map((item) => {
+				if (item.sliderName == id) {
+					return { ...item, fungsiValue: value };
+				}
+				return item;
+			})
+		);
+	};
 
 	useEffect(() => {
 		axios
@@ -34,7 +34,9 @@ const GeneratorRelationship = () => {
 
 	return (
 		<div className="m-10 flex flex-col gap-20">
-			<div className="absolute top-10 left-10 flex">
+			<div className="top-15 absolute left-20 flex"></div>
+
+			<div className="mx-auto flex gap-10 items-center">
 				<Link to="/select-generator">
 					<p
 						className="rounded-xl border-2 border-[#D9D9D9] bg-[#010440] p-2 text-center text-2xl hover:border-[#010440] hover:bg-[#D9D9D9] hover:text-[#010440]"
@@ -43,12 +45,18 @@ const GeneratorRelationship = () => {
 						{"< Back"}
 					</p>
 				</Link>
-			</div>
-
-			<div className="mx-auto flex">
-				<p className="mt-4 text-center text-2xl lg:text-4xl">
+				<p className="text-center text-2xl lg:text-4xl">
 					Testing the Generator
 				</p>
+			</div>
+			<div className="flex items-center justify-center">
+				<div className="flex h-96 w-[50rem] rounded-2xl bg-cyan-600">
+					<img
+						className="flex h-96 w-[50rem] rounded-2xl object-cover"
+						src={bgImage}
+						alt="bg-image"
+					/>
+				</div>
 			</div>
 			<div className="flex flex-row gap-10">
 				<div className="flex w-1/2 flex-col gap-5">
@@ -70,19 +78,6 @@ const GeneratorRelationship = () => {
 							maxLength={32}
 							onChange={handleChange}
 						/>
-					</div>
-					<p className="flex justify-center text-center text-2xl underline">
-						Upload Your OC PP
-					</p>
-					<div className="flex h-auto flex-col items-center justify-center gap-5 border-4 border-cyan-400 p-5 text-center">
-						<p className="text-center">
-							*Please upload with PNG, JPEG or JPG format only
-							<br />
-							**Recommend to upload image with 1:1 ratio
-							<br />
-							***Max Size is 2MB
-						</p>
-						<AddPP />
 					</div>
 					<p className="flex justify-center text-center text-2xl underline">
 						Set Your OC's
@@ -107,9 +102,6 @@ const GeneratorRelationship = () => {
 					<div className="flex flex-col items-center justify-center gap-10 border-4 border-cyan-400 p-5">
 						<ul className="flex flex-col text-center">
 							<li className="mb-5">Name : {nameInput}</li>
-							<li>
-								PP Image : <img src={"data_url"} alt="" />
-							</li>
 							{slideValue.map((slider, index) => (
 								<li key={index}>
 									{slider.sliderTitle}
